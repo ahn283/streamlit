4a431ed4307ebd8ac7decd990c9ca0230d8798c4
ahn283 <ahn283@gmail.com>
2023-07-08 16:52:23 +0900

    Add files via upload

---
+++ technical_analysis_app.py
@@ -0,0 +1,174 @@
+# imports
+import yfinance as yf
+import streamlit as st
+import datetime
+import pandas as pd
+import cufflinks as cf
+from plotly.offline import iplot
+
+# set offline mode for cufflinks
+cf.go_offline()
+
+# dsta functions
+@st.cache
+def get_sp500_components():
+    """Get SP500 components from Wikipedia"""
+    # get table from Wikipedia
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    df = pd.read_html(url)
+    df = df[0]
+    
+    tickers = df['Symbol'].tolist()
+    tickers_companies_dict = dict(
+        zip(df['Symbol'], df['Security'])
+    )
+    return tickers, tickers_companies_dict
+
+@st.cache
+def load_data(symbol, start, end):
+    """Load data from Yahoo Finance"""
+    df = yf.download(symbol, start, end)
+    return df
+
+@st.cache
+def convert_df_to_csv(df):
+    """Convert dataframe to csv"""
+    return df.to_csv().encode('utf-8')
+
+# sidebar
+
+## inputs for downloading data
+st.sidebar.header('Stock Parameters')
+
+available_tickers, tickers_companies_dict = get_sp500_components()
+
+ticker = st.sidebar.selectbox(
+    'Ticker',
+    available_tickers,
+    format_func=tickers_companies_dict.get
+)
+
+start_date = st.sidebar.date_input(
+    'Start date',
+    datetime.date(2019, 1, 1)
+)
+
+end_date = st.sidebar.date_input(
+    'End date',
+    datetime.date.today()
+)
+
+if start_date > end_date:
+    st.sidebar.error('Error: The end date must fall after start date.')
+
+## inputs for technical analysis
+st.sidebar.header('Technical Analysis Parameters')
+
+volume_flag = st.sidebar.checkbox(label='Add volume')
+
+exp_sma = st.sidebar.expander('SMA')
+sma_flag = exp_sma.checkbox(label='Add SMA')
+sma_period = exp_sma.number_input(
+    label='SMA Period',
+    min_value=1,
+    max_value=50,
+    value=20,
+    step=1
+)
+
+exp_bb = st.sidebar.expander('Bollinger Bands')
+bb_flag = exp_bb.checkbox(label='Add Bollinger Bands')
+bb_period = exp_bb.number_input(
+    label='Bollinger Bands Period',
+    min_value=1,
+    max_value=50,
+    value=20,
+    step=1
+)
+
+bb_std = exp_bb.number_input(
+    label='# of standard deviations',
+    min_value=1,
+    max_value=4,
+    value=2,
+    step=1
+)
+
+exp_rsi = st.sidebar.expander('Relative Strength Index')
+rsi_flag = exp_rsi.checkbox(label='Add RSI')
+rsi_period = exp_rsi.number_input(
+    label='RSI Period',
+    min_value=1,
+    max_value=50,
+    value=20,
+    step=1
+)
+rsi_upper = exp_rsi.number_input(
+    label='RSI Upper',
+    min_value=50,
+    max_value=90,
+    value=70,
+    step=1
+)
+rsi_lower = exp_rsi.number_input(
+    label='RSI Lower',
+    min_value=10,
+    max_value=50,
+    value=30,
+    step=1
+)
+
+# main body
+
+st.title('A simple web app for technical analysis')
+st.write("""
+        ### User manual
+        * you can select any of the companies that is a component of the S&P index
+        * you can select the time period of your interest
+        * you can download the selected data as a CSV file
+        * you can add the following Technical Indicators to the plot: Simple Average, Bollinger Bands, Relative Strength Index
+        * you can experiment with different parameters of the indicators
+         """)
+
+df = load_data(ticker, start_date, end_date)
+
+## data preview part
+data_exp = st.expander('Preview data')
+available_cols = df.columns.tolist()
+columns_to_show = data_exp.multiselect(
+    "Columns",
+    available_cols,
+    default=available_cols
+)
+data_exp.dataframe(df[columns_to_show])
+
+csv_file = convert_df_to_csv(df[columns_to_show])
+data_exp.download_button(
+    label='Download data as CSV',
+    data=csv_file,
+    file_name=f'{ticker}_stock_prices.csv',
+    mime='text/csv'
+)
+
+## technical analysis plot
+title_str = f"{tickers_companies_dict[ticker]}'s stock price"
+qf = cf.QuantFig(df, title=title_str)
+
+if volume_flag:
+    qf.add_volume()
+if sma_flag:
+    qf.add_sma(periods=sma_period)
+if bb_flag:
+    qf.add_bollinger_bands(
+        periods=bb_period,
+        boll_std=bb_std
+    )
+if rsi_flag:
+    qf.add_rsi(
+        periods=rsi_period,
+        rsi_upper=rsi_upper,
+        rsi_lower=rsi_lower,
+        showbands=True
+    )
+fig = qf.iplot(asFigure=True)
+st.plotly_chart(fig)
